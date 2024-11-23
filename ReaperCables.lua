c0540360c587ff446559742403753ab01ec769a6
Oscar Yang <oscaryangb@gmail.com>
2024-11-23 11:34:12 -0500

    Track names

--- ReaperCables.lua
+++ ReaperCables.lua
@@ -3,28 +3,47 @@ navY = 0
 isDragging = false
 dragX = 0
 dragY = 0
-gfx.init("Test",1000,750,0,0,0)
+
+Block = {x = 0, y = 0, name = ""}
+
+function Block:new (x,y,name)
+  block = {}
+  block.x = x
+  block.y = y
+  block.name = name
+  return block
+end
+
+blocks = {}
 
 function getAllTrackNames()
   list = {}
   for i=0,reaper.GetNumTracks()-1 do
     track = reaper.GetTrack(0,i)
-    a, list[i] = reaper.GetTrackName(track)
+    a, list[i+1] = reaper.GetTrackName(track)
   end
   
   return list
 end
 
+function toMenuString(strings)
+  text = ""
+  
+  for i=1, #strings do
+    text = text .. strings[i] .. "|"
+  end
+  
+  return text
+end
 
-function drawBlock(x,y)
+
+function drawBlock(x,y,name)
   width = 200
   height = 150
   gfx.roundrect(x+navX,y+navY,width,height,10,true)
   gfx.x = x+navX+width/3
   gfx.y = y+navY
-  a = false
-  list = getAllTrackNames()
-  gfx.drawstr(list[1])
+  gfx.drawstr(name)
 end
 
 function update()
@@ -41,11 +60,25 @@ function update()
     dragY = gfx.mouse_y
   end
   
-  drawBlock(100,100)
+  if gfx.mouse_cap == 2 then
+    gfx.x = gfx.mouse_x
+    gfx.y = gfx.mouse_y
+    trackNames = getAllTrackNames()
+    t = gfx.showmenu(toMenuString(trackNames))
+    if t > 0 then
+      table.insert(blocks, Block:new(gfx.mouse_x, gfx.mouse_y, trackNames[t]))
+    end
+  end
+  
+  for i = 1, #blocks do
+    drawBlock(blocks[i].x, blocks[i].y, blocks[i].name)
+  end
+  
   gfx.update()
   reaper.runloop(update)
 end
 
+gfx.init("Test",1000,750,0,0,0)
 gfx.setfont(1,"Calibri", 20, "")
 update()
 
